bacdb0fbdaa474afac5f7e401c75338690e81ce2
salgu1998 <salgu1998@gmail.com>
2021-11-21 12:44:34 +0900

    traceId 동기화

---
+++ src/test/java/hello/advanced/trace/logtrace/FieldLogTraceTest.java
@@ -0,0 +1,31 @@
+package hello.advanced.trace.logtrace;
+
+import hello.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FieldLogTraceTest {
+
+    FieldLogTrace trace = new FieldLogTrace();
+
+    @Test
+    void begin_end_level2() {
+        TraceStatus status = trace.begin("Hello1");
+        TraceStatus status2 = trace.begin("Hello2");
+        TraceStatus status3 = trace.begin("Hello3");
+        trace.end(status3);
+        trace.end(status2);
+        trace.end(status);
+    }
+
+    @Test
+    void begin_exception_level2() {
+        TraceStatus status = trace.begin("Hello1");
+        TraceStatus status2 = trace.begin("Hello2");
+        TraceStatus status3 = trace.begin("Hello3");
+        trace.exception(status3, new IllegalStateException());
+        trace.exception(status2, new IllegalStateException());
+        trace.end(status);
+    }
+}
